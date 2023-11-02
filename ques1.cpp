dc113563b95e7c2e510effd2d42f2881e0226d71
Rohit_1527 <rohit.kumar@dotnova.ai>
2023-11-02 23:14:09 +0530

    even odd number question

---
+++ ques1.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "enter the value of n"<<endl;
+    cin>>n;
+    if(n % 2 == 0){
+        cout<<" this is the even number"<<endl;
+    }
+    else{
+     cout<< "this is the odd number"<<endl;
+    }
+}
