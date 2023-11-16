00f4b9da3f4c4863ceb6a648a7a0d8aaa8b98ce9
ArunRai2021 <arunraihrit@gmail.com>
2023-11-16 15:37:15 +0530

    swaping of two number

---
+++ swap_two_number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 5 , b= 10 ,temp;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << " after swaping of these Two numbers " << a << " and "<< b << endl;
+
+}
