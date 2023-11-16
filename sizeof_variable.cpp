b9c8ab5a2f0ec21639f9a3bddd39b4f349a01d3b
Rohit_1527 <rohit.kumar@dotnova.ai>
2023-11-16 12:30:46 +0530

    size of variable code

---
+++ sizeof_variable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"size of charecter is "<<sizeof(char) << " bytes"<<endl;
+    cout<<"size of int is "<<sizeof(int)<< " Bytes"<<endl;
+    cout<<"size of float is "<<sizeof(float)<< " Bytes"<<endl;
+    cout<<"size of double is "<<sizeof(double)<< " Bytes"<<endl;
+    return 0;
+
+}
