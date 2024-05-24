82b0148796e644c92f451b89a86af416c11011fe
ArunRai2021 <arunraihrit@gmail.com>
2024-05-24 15:45:13 +0530

    all code are commited

---
+++ number_greater_or_less.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+ using namespace std;
+ int main(){
+    int n;
+    cout<< "enter the value of n"<<endl;
+    cin>>n;
+if(n == 1){
+    cout<<"one";
+}
+else if(n == 2){
+    cout<<"two";
+}
+else if(n== 3){
+    cout<<"three";
+}
+
+ }
